989b46095a93362a7773e586ee3c60f0aa367fdc
DimaSv <dimasvjet@gmail.com>
2022-12-31 07:30:48 +0200

    Home work 12 finished

--- src/Home_Work_12.java
+++ src/Home_Work_12.java
@@ -4,18 +4,19 @@ import java.util.Scanner;
 public class Home_Work_12 {
     public static void main(String[] args) {
         String personCall;
-        String personCall1;
-       Person person1 = new Person("Will","Smith", "New York", "2936729462846");
-        personCall = Person.personInfo("Will","Smith", "New York", "2936729462846"); //a
-        personCall1 = person1.personInfo("Will","Smith", "New York", "2936729462846");//b
-        System.out.println("a. " +personCall);
-        System.out.println("b. " +personCall1);
-        System.out.println("c. " +person1);
 
-       Person person2 = new Person("Jackie", "Chan", "Shanghai", "12312412412");
-       //System.out.println(person2);
-       Person person3 = new Person("Sherlock", "Holmes", "London", "37742123513");
-       //System.out.println(person3);
+        personCall = Person.personInfo("Will","Smith", "New York", "2936729462846");
+        System.out.println("a. " +personCall);
+
+
+        personCall = Person.personInfo("Jackie", "Chan", "Shanghai", "12312412412");
+        System.out.println("b. " +personCall);
+
+
+
+        personCall = Person.personInfo("Sherlock", "Holmes", "London", "37742123513");
+        System.out.println("c. " +personCall);
+
     }
 }
 
--- src/Person.java
+++ src/Person.java
@@ -1,16 +1,5 @@
 public class Person {
 
-    public String name;
-    public String lastName;
-    public String city;
-    public long mob;
-
-    public Person(String name, String lastName, String city, String mob) {
-        this.name = name.trim();
-        this.lastName = lastName.trim();
-        this.city = city.trim();
-        this.mob = Long.parseLong(mob.trim());
-    }
 
     public static String personInfo(String name, String lastName, String city, String mob){
        String personInform = "Зателефонувати громадянинові " + name.trim() + " " + lastName.trim() + " із міста " +
@@ -19,12 +8,4 @@ public class Person {
     }
 
 
-    @Override
-    public String toString() {
-        return "Зателефонувати громадянинові " +
-                name +
-                " " + lastName + " із міста " +
-                city +
-                " можна за номером: " + mob;
-    }
 }
